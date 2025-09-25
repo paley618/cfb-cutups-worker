6264f728fe8445351b4dc0edb7cdcab87b08e051
paley618 <grantapaley@gmail.com>
2025-09-24 21:54:26 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,4 +9,4 @@ echo "Python version:"
 python --version
 
 echo "Attempting to run Uvicorn..."
-exec uvicorn main:app --host 0.0.0.0 --port 8000 --log-level debug
+exec uvicorn main:app --host 0.0.0.0 --port ${PORT:-8000} --log-level debug
