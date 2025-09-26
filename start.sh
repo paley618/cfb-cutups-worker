41907e0218785ace4f73e60107824cec0c9b21ae
paley618 <grantapaley@gmail.com>
2025-09-26 10:10:32 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,3 @@ set -e
 
 echo "Starting FastAPI app with uvicorn..."
 uvicorn main:app --host 0.0.0.0 --port 8000
-
-git update-index --chmod=+x start.sh
-git commit -m "Ensure start.sh is executable"
-git push
